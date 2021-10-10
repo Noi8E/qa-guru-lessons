6bda3c96337cb606cf8e065d15b149b4722f5922
Noi8E <ftretyakov@kosmostyt.ru>
2021-10-10 18:33:12 +0300

    redact TestBse

---
+++ src/test/java/properties/SystemPropsExample.java
@@ -0,0 +1,30 @@
+package properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import tests.TestBase;
+
+public class SystemPropsExample extends TestBase {
+//    @Test
+//    @Tag("props")
+//    void someTest() {
+//        String val = System.getProperty("value");
+//    }
+//    @Test
+//    @Tag("props")
+//    void someTest1() {
+//        System.setProperty("value", "an_value");
+//        String val = System.getProperty("value","def_props");
+//        System.out.println(val);
+//    }
+//    @Test
+//    @Tag("props")
+//    void someTest2() {
+//        String val = System.getProperty("browser","chrome");
+//        String val2 = System.getProperty("version", "90");
+//        System.out.println("browser is" + val);
+//        System.out.println("version is" + val2);
+//        System.out.println("remoteAddress is " + selenoidRemoteAddress);
+//    }
+
+}
