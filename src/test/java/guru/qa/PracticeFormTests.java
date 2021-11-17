b406fc55b568edfbe140787f9ba60f907e0b79a6
Arkadiy <arkadiy.garanin@socialsys.ru>
2021-11-18 00:42:02 +0300

    1) Добавил HashMap с ожидаемыми данными 2) Переименовал название теста 3) Удалил метод beforeAll(), мне кажется он лишний, т.к тест класс и так наследует TestBase в котором этот метод есть 4) Добавил параметр методу setGender() 5) Добавил параметр методу checkFinalTable()

--- src/test/java/guru/qa/PracticeFormTests.java
+++ src/test/java/guru/qa/PracticeFormTests.java
@@ -1,8 +1,5 @@
 package guru.qa;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 import guru.qa.pages.RegistrationPage;
 import guru.qa.pages.components.CalendarComponent;
 import guru.qa.pages.components.HobbiesComponent;
@@ -13,9 +10,6 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
-
 public class PracticeFormTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
@@ -23,40 +17,55 @@ public class PracticeFormTests extends TestBase {
     SubjectComponent subject = new SubjectComponent();
     HobbiesComponent hobbiesComponent = new HobbiesComponent();
 
-    @Test
-    void canSubmitForm() {
+    Map<String, String> expectedData = new HashMap<>() {{
+        put("Student Name", firstName + " " + lastName);
+        put("Student Email", userEmail);
+        put("Gender", studentGender);
+        put("Mobile", phoneNumber);
+        put("Date of Birth", dayCalendar + " " + monthCalendar + "," + yearCalendar);
+        put("Subjects", subjectValue1 + ", " + subjectValue2);
+        put("Hobbies", rndHobbies1 + ", " + rndHobbies2);
+        put("Picture", picture);
+        put("Address", rndAddres);
+        put("State and City", rndState + " " + rndCity);
+    }};
 
-        beforeAll();
+    @Test
+    void registrationFormTest() {
+
         registrationPage.openPage();
+
         registrationPage.typeFirstName(firstName);
         registrationPage.typeLastName(lastName);
         registrationPage.typeEmail(userEmail);
-        registrationPage.setGender();
+        registrationPage.setGender(studentGender);
         registrationPage.setPhoneNumber(phoneNumber);
-        calendar.setDate(dayCalendar,monthCalendar,yearCalendar);
-        subject.setSubject(subjectValue1,subjectValue2);
-        hobbiesComponent.setHobbies(rndHobbies1,rndHobbies2);
+        calendar.setDate(dayCalendar, monthCalendar, yearCalendar);
+        subject.setSubject(subjectValue1, subjectValue2);
+        hobbiesComponent.setHobbies(rndHobbies1, rndHobbies2);
         registrationPage.getUploadPicture(new File("src/test/resources/BoJack-on-Behance.png"));
         registrationPage.setAddress(rndAddres);
         registrationPage.setState(rndState);
         registrationPage.setCity(rndCity);
+
         registrationPage.getSubmit();
 
         //Проверяем что окно с информацией отображается
         registrationPage.checkModalWindowHeader();
+
         //Проверка корректности введенных данных
+        registrationPage.checkFinalTable(expectedData);
         // Хотел через Map, но так и не понял - как это сделать.
-        registrationPage.checkFinalTable("Student Name", firstName + " " + lastName);
-        registrationPage.checkFinalTable("Student Email", userEmail);
-        registrationPage.checkFinalTable("Gender", studentGender);
-        registrationPage.checkFinalTable("Mobile", phoneNumber);
-        registrationPage.checkFinalTable("Date of Birth", dayCalendar + " " + monthCalendar + "," + yearCalendar);
-        registrationPage.checkFinalTable("Subjects", subjectValue1 + ", " + subjectValue2 );
-        registrationPage.checkFinalTable("Hobbies", rndHobbies1 + ", " + rndHobbies2);
-        registrationPage.checkFinalTable("Picture", "BoJack-on-Behance.png");
-        registrationPage.checkFinalTable("Address", rndAddres);
-        registrationPage.checkFinalTable("State and City", rndState + " " + rndCity);
-
-
+//        registrationPage.checkFinalTable("Student Name", firstName + " " + lastName);
+//        registrationPage.checkFinalTable("Student Email", userEmail);
+//        registrationPage.checkFinalTable("Gender", studentGender);
+//        registrationPage.checkFinalTable("Mobile", phoneNumber);
+//        registrationPage.checkFinalTable("Date of Birth", dayCalendar + " " + monthCalendar + "," +
+//                yearCalendar);
+//        registrationPage.checkFinalTable("Subjects", subjectValue1 + ", " + subjectValue2 );
+//        registrationPage.checkFinalTable("Hobbies", rndHobbies1 + ", " + rndHobbies2);
+//        registrationPage.checkFinalTable("Picture", "BoJack-on-Behance.png");
+//        registrationPage.checkFinalTable("Address", rndAddres);
+//        registrationPage.checkFinalTable("State and City", rndState + " " + rndCity);
     }
 }
